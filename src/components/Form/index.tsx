2babb5a022cb7a876bc2b75fa85322724c26c274
Victor Hugo <83098581+lkt226@users.noreply.github.com>
2023-09-05 12:41:10 -0300

    ajustes de usabilidade

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -3,33 +3,52 @@ import css from './form.module.scss'
 
 import FormCard from './Card'
 import ConfigCard from './Config'
+import { useSelector } from 'react-redux'
+
+import { CompanyStateType } from '../../redux/company/slice'
+import { RootState } from '../../redux/store'
 
 const Form = () => {
-  const [statusConfig, setStatusConfig] = useState(false)
+  let initialStatusConfig = true;
+  const company = useSelector((rootReducer: RootState) => rootReducer.companyReducer) as CompanyStateType;
+  
+  const { company_name, company_cost, day_hours, employees, days_in_week } = company;
+  if (!company_name || !company_cost || !day_hours || !employees || !days_in_week ) {
+    initialStatusConfig = true;
+  } else {
+    initialStatusConfig = false;
+  }
+
+  const [statusConfig, setStatusConfig] = useState(initialStatusConfig)
+
 
   const FormBox = () => (
-    <>
+    <div className={css["form"]}>
       <div className={css["header"]}>
         <h2>Novo calculo</h2>
         <button onClick={() => setStatusConfig(true)} >⚙️</button>
       </div>
       <FormCard />
-    </>
+    </div>
   )
 
   const ConfigBox = () => (
     <>
-      <div className={css["header"]}>
-        <h2>Sobre a empresa</h2>
-        <button onClick={() => setStatusConfig(false)}>✖️</button>
+      <div className={css["form-config"]}>
+        <div className={css["header"]}>
+          <h2>Sobre a empresa</h2>
+          <button onClick={() => setStatusConfig(false)}>✖️</button>
+        </div>
+        <ConfigCard handleClose={() => setStatusConfig(false)} />
       </div>
-      <ConfigCard handleClose={() => setStatusConfig(false)} />
+      <div className={css["background-form"]} />
     </>
   )
 
   return (
       <div className={css["form"]}>
-        { statusConfig ? <ConfigBox /> : <FormBox /> }
+        { statusConfig && <ConfigBox /> }
+        <FormBox />
       </div>
   )
 }
